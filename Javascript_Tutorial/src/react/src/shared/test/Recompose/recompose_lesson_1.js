50ac19d486a7f1462b210edc64c63201a28f7331
daint2git <peterpoy93@gmail.com>
2018-04-13 00:04:48 +0700

    DaiNT2 update recompose_lesson_1

--- Javascript_Tutorial/src/react/src/shared/test/Recompose/recompose_lesson_1.js
+++ Javascript_Tutorial/src/react/src/shared/test/Recompose/recompose_lesson_1.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withProps, compose } from 'recompose'
+import { withState, withProps, compose } from 'recompose'
 
 const ComponentTest = props => {
   console.log('props', props)
@@ -8,7 +8,17 @@ const ComponentTest = props => {
   )
 }
 
-const ComponentTestEnhancer = compose(
+const enhancer = compose(
+  withState('value1', 'changeValue1', props => {
+    console.log('withState1')
+    console.log('withState1 props', props)
+    return { a: 'string', b: true, c: 99 }
+  }),
+  withState('value2', 'changeValue2', props => {
+    console.log('withState2')
+    console.log('withState2 props', props)
+    return { array: [1, 2, 3] }
+  }),
   withProps(({ x = 1 }) => {
     console.log('withProps 1')
     return { x: x + 2 }
@@ -16,7 +26,13 @@ const ComponentTestEnhancer = compose(
   withProps(({ x }) => {
     console.log('withProps 2')
     return { x: x * 2 }
+  }),
+  withProps(({ x }) => {
+    console.log('withProps 3')
+    return {
+      total: x + 10
+    }
   })
 )(ComponentTest)
 
-export default ComponentTestEnhancer
+export default enhancer
